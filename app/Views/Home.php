b193cae6b1de222f00bfdd5ce3fcf16ef0d68dd1
Miguel Lacruz <malacruz132@gmail.com>
2025-11-07 18:23:41 -0500

    Modificada el Home, tambien agregado el layout y algunas cositas!!!

--- app/Views/Home.php
+++ app/Views/Home.php
@@ -1,1 +1,50 @@
-<h1>Pagina inicial</h1>
+<?php echo $this -> extend("plantilla/layout")?>
+<?php echo $this -> section("contenido")?>
+
+<h1 class="border  p-2">Crea, protege y disfruta a bajos costos!!!</h1>
+
+<div class="border p-5 block d-lg-block container-xl">
+    <p1>
+        Crear servidores host en colombia nunca fue tan facil con HostColombiano
+    </p1>
+    <br>
+    <p1>
+        Facil, rapido, seguro y a los mejores precios.
+    </p1>
+    <br>
+    <p1>
+        ¿Que estas esperando para armar tu servidor en Colombia?
+    </p1>
+</div>
+
+<a class="btn btn-success rounded-pill">Crear cuenta gratis</a>
+
+
+<div class="d-flex justify-content-center">
+    <div class="border m-2 p-2">
+        <h3 class="text-center">¿Porque elegirnos?</h3>
+        <p>
+            Nuestro servicio busca ser facil de usar y <br>
+            muy accesible para aquellos que desean montar <br>
+            sus servidores en Colombia.
+        </p>
+    </div>
+    <div class="border m-2 p-2">
+        <h3 class="text-center">Facil uso</h3>
+        <p>
+            No nesecitas experiencia previa para usar <br>
+            nuestros servicios,<br>
+            gracias a la interfazy la documentacion, <br>
+            puedes usar facilmente el servicio.
+        </p>
+    </div>
+    <div class="border m-2 p-2">
+        <h3 class="text-center">Mejor opcion</h3>
+        <p>
+            EL servicio es altamente accesible, <br>
+            facil uso y confiable para todos aquellos <br>
+            que buscan montar sus servidores y HOST en Colombia.
+        </p>
+    </div>
+</div>
+<?php echo $this -> endSection()?>
