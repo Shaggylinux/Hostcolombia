604687a90f519ecb710264b507cf31b64e7d13fa
Miguel Lacruz <malacruz132@gmail.com>
2025-11-19 19:36:48 -0500

    Agregada la base de datos .sql

---
+++ BaseDatos.sql
@@ -0,0 +1,26 @@
+create database proyecto;
+
+create table perfil(
+	id int primary key auto_increment,
+	descripcion text
+);
+
+create table usuarios(
+	id int primary key auto_increment,
+	nombre varchar(20),
+	nombreusuario varchar(20),
+	clave text,
+	correo text,
+	id_perfil int
+	foreign key (id_perfil) references perfil(id)
+);
+
+create table servidor(
+	id int primary key auto_increment,
+	nombre varchar(45),
+	descripcion varchar(50),
+	dominio varchar(30),
+	dns varchar(20),
+	id_usuario int
+	foreign key (id_usuario) references usuarios(id)
+);
