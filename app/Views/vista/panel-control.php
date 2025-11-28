18cc93b5d3c222a8fd01550c35d293c71d33d5d5
Miguel Lacruz <malacruz132@gmail.com>
2025-11-27 20:51:19 -0500

    alguna modificacion que hice que ya no me acuerdo jaja

--- app/Views/vista/panel-control.php
+++ app/Views/vista/panel-control.php
@@ -8,18 +8,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panel <?php  echo $servidor["id"]?></title>
 </head>
-<body>
-    <h1>Panel de control</h1>
-<h1><?php echo $servidor["nombre"] ?></h1>
-<a href="https://<?php echo $servidor["dominio"] ?>"> click</a>
-
-<?php if ($online): ?>
-    <span class="text-success fw-bold">ONLINE</span>
-<?php else: ?>
-    <span class="text-danger fw-bold">OFFLINE</span>
-<?php endif; ?>
-
-</body>
+    <body>
+        <h1>Panel de control</h1>
+    <h1><?php echo $servidor["nombre"] ?></h1>
+    <div>
+        <?php if ($online): ?>
+            <span class="text-success fw-bold">ONLINE</span>
+            <a href="https://<?php echo $servidor["dominio"] ?>"> IR : <?php echo $servidor["dominio"] ?>  </a>
+            <?php else: ?>
+            <span class="text-danger fw-bold">OFFLINE</span>
+            <?php endif;?>
+    </div>
+    </body>
 </html>
 
 <?php echo $this -> endSection()?>
